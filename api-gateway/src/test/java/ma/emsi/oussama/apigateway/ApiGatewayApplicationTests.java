79b0c210adf8ee1ebd7938ae3b78216d331f45f4
oussama Essalmani <essalmanioussama@gmail.com>
2025-11-28 12:05:35 +0100

    add getaway + server

---
+++ api-gateway/src/test/java/ma/emsi/oussama/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.emsi.oussama.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
