1ee6c9eb41b7fb793dc14eafaf0eaa5eb2cc930d
oussama Essalmani <essalmanioussama@gmail.com>
2025-12-12 00:30:56 +0100

    fix resrvation error

--- hotel-service/src/main/java/ma/emsi/oussama/hotelservice/entities/Chambre.java
+++ hotel-service/src/main/java/ma/emsi/oussama/hotelservice/entities/Chambre.java
@@ -2,6 +2,7 @@ package ma.emsi.oussama.hotelservice.entities;
 
 import ma.emsi.oussama.hotelservice.enums.TypeChambre;
 import jakarta.persistence.*;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,9 +10,12 @@ import lombok.NoArgsConstructor;
 import java.math.BigDecimal;
 
 @Entity
-@Data @NoArgsConstructor @AllArgsConstructor
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Chambre {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String numero;
     @Enumerated(EnumType.STRING)
@@ -19,6 +23,7 @@ public class Chambre {
     private BigDecimal prixParNuit;
     private Boolean disponible;
 
+    @JsonIgnore
     @ManyToOne
     @JoinColumn(name = "hotel_id")
     private Hotel hotel;
